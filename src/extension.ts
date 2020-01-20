84b6691f484a0e7da633c31fd8da8a9b911b6627
Mikhail Remmele <mikhail.remmele@gmail.com>
2020-01-20 22:07:02 +0600

    fixed regexp using for replace params in index.html to values like content or media path. Fixed base path scheme from resource to vscode-resource. Added styles implemented while task 1

--- src/extension.ts
+++ src/extension.ts
@@ -50,7 +50,7 @@ const getPreviewKey = (doc: vscode.TextDocument): string => doc.uri.path;
 
 const getMediaPath = (context: vscode.ExtensionContext) => vscode.Uri
     .file(context.extensionPath)
-    .with({ scheme: "resource"})
+    .with({ scheme: "vscode-resource"})
     .toString() + '/';
 
 const initPreviewPanel = (document: vscode.TextDocument) => {
@@ -87,7 +87,7 @@ const updateContent = (doc: vscode.TextDocument, context: vscode.ExtensionContex
             const html = template.apply(data);
 
             panel.webview.html = previewHtml 
-                .replace(/{{\s+(\w+)\s+}}/g, (str, key) => {
+                .replace(/{{\s*(\w+)\s*}}/g, (str, key) => {
                     switch (key) {
                         case 'content':
                             return html;
